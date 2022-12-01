1d55e6293d6db49bcd04493a6fba9a06a27098e4
David Wrenner <davidwrenner@wustl.edu>
2022-12-01 14:37:35 -0600

    adjust spacing

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import os
 input_file = sys.argv[0][0:2] + ".txt"
 
 if not os.path.exists(input_file):
-	sys.exit(f"Did not find expected file {input_file}") 
+    sys.exit(f"Did not find expected file {input_file}")
 
 with open(input_file, "r") as f:
     input = [line.rstrip() for line in f if line != "\n"]
