0d364056b0533ad09f36ea5d2e21c921ab7403bb
David Wrenner <davidwrenner@wustl.edu>
2022-12-01 19:19:57 -0600

    december 1 done

--- src/00.py
+++ src/00.py
@@ -8,18 +8,24 @@ input_path = "../input/" + input_file
 if not os.path.exists(input_path):
     sys.exit(f"Did not find expected input {input_path}")
 
-with open(input_file, "r") as f:
-    input = [line.rstrip() for line in f if line != "\n"]
+with open(input_path, "r") as f:
+    input = [line.rstrip() for line in f]
 
 ans = None
 # part a
-
+elf = 0
+elves = []
+for line in input:
+    if line == "":
+        elves.append(elf)
+        elf = 0
+    else:
+        elf += int(line)
+ans = max(elves)
 print("Answer A:", ans)
 
 # part b
- 
+elves.sort()
+ans = sum(elves[-3:])
 
 print("Answer B:", ans)
-
-
- 
