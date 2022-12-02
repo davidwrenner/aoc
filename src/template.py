5747ee90d6d3fefc02f71947f9bc19ef31384f1c
David Wrenner <davidwrenner@wustl.edu>
2022-12-02 11:15:54 -0600

    update template

--- src/template.py
+++ src/template.py
@@ -6,7 +6,7 @@ input_file = sys.argv[0][0:2] + ".txt"
 input_path = "../input/" + input_file
 
 if not os.path.exists(input_path):
-    sys.exit(f"Did not find expected input {input_path}")
+    raise FileNotFoundError(f"Did not find expected input {input_path}")
 
 with open(input_path, "r") as f:
     input = [line.rstrip() for line in f if line != "\n"]
