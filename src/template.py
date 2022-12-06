f82814b7c8e7cb3a3bc31c249180c0dddb710f28
David Wrenner <davidwrenner@wustl.edu>
2022-12-06 09:37:07 -0600

    update template

--- src/template.py
+++ src/template.py
@@ -1,12 +1,8 @@
+#!/usr/bin/env python3
 import sys
 import os
 
-# file I/O
-input_file = sys.argv[0][0:2] + ".txt"
-input_path = "../input/" + input_file
-
-if not os.path.exists(input_path):
-    raise FileNotFoundError(f"Did not find expected input {input_path}")
+input_path = "../input/" + __file__.replace('py', 'txt')
 
 with open(input_path, "r") as f:
     input = [line.rstrip() for line in f]
