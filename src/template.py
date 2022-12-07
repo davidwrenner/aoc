6012e38e6e4affd40b56f099d7fdecd548ce4fdf
David Wrenner <davidwrenner@wustl.edu>
2022-12-07 10:47:40 -0600

    update template

--- src/template.py
+++ src/template.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-import sys
 import os
+import subprocess as sp
+import sys
 
+ans = None
 input_path = "../input/" + __file__.replace('py', 'txt')
-
 with open(input_path, "r") as f:
     input = [line.rstrip() for line in f]
 
-ans = None
 # part a
 
 print("Answer A:", ans)
 
 # part b
- 
 
 print("Answer B:", ans)
-
-
- 
+sp.run("pbcopy", input=ans, text=True)
