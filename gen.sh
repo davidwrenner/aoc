fc9263b615d5efae03ccfad5421c1190db0ce371
David Wrenner <davidwrenner@wustl.edu>
2023-10-31 23:52:42 -0400

    cleanup & make shellcheck happy

--- gen.sh
+++ gen.sh
@@ -3,15 +3,14 @@
 
 num_days_of_aoc=25
 
-source config
+source "config"
 if [[ -z "$session_cookie" ]]; then
-    echo \[FATAL\] session_cookie not set
-    exit 1
+    echo "[FATAL] session_cookie not set"; exit 1
 fi
 
 year=$(date +%Y)
 year_src="$year/src"
-cd "$year_src" || { echo [FATAL\] not configured for "$year"; exit 2; }
+cd "$year_src" || { echo "[FATAL] not configured for $year"; exit 2; }
 
 # find the next unsolved puzzle
 day=1
@@ -20,8 +19,7 @@ while [ -e "$py" ]; do
     printf -v py '%02d.py' "$(( ++day ))"
 done
 if [ "$day" -gt "$num_days_of_aoc" ]; then
-    echo \[FATAL\] day "$day" geq the number of aoc days "$num_days_of_aoc"
-    exit 3
+    echo "[FATAL] day $day geq the number of aoc days $num_days_of_aoc"; exit 3
 fi
 printf -v txt '%02d.txt' $day
 
@@ -29,14 +27,11 @@ curl "https://adventofcode.com/$year/day/$day/input" \
     -X "GET" \
     -H "Cookie: session=$session_cookie" >> "$txt"
 if [[ ! -f "$txt" ]]; then
-    echo \[FATAL\] "failed to curl input"
-    exit 4
+    echo "[FATAL] failed to curl input"; exit 4
 fi
 
-mv "$txt" ../input/ && cp template.py "$py"
-if [ $? -eq 0 ]; then
-    echo \[INFO\] generated "$py" and "$txt"
+if mv "$txt" "../input/" && cp "template.py" "$py"; then
+    echo "[INFO] generated $py and $txt"
 else
-    echo \[FATAL\] something happened while generating "$py" and "$txt"
-    exit 5
+    echo "[FATAL] something happened while generating $py and $txt"; exit 5
 fi
