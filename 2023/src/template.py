ae2e75a8dadc7933278fc4c41eeb44b052c1f8f2
David Wrenner <davidwrenner@wustl.edu>
2023-12-02 17:10:45 -0500

    update template

--- 2023/src/template.py
+++ 2023/src/template.py
@@ -2,10 +2,12 @@
 import os
 import subprocess as sp
 import sys
-from numpy import inf
+import numpy as np
+import re
 
 ans = None
-input_path = "../input/" + __file__.replace('py', 'txt')
+input_path = "../input/" + __file__.replace('py', 'txt') if "-t" not in sys.argv else "testcase.txt"
+
 with open(input_path, "r") as f:
     input = [line.rstrip() for line in f]
 
