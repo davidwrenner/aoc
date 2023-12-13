916734386d9b27a9902a5b8a81c34ce880655d8c
David Wrenner <davidwrenner@wustl.edu>
2023-12-13 18:54:43 -0500

    template timing

--- 2023/src/template.py
+++ 2023/src/template.py
@@ -4,7 +4,9 @@ import subprocess as sp
 import sys
 import numpy as np
 import re
+from datetime import datetime
 
+print("Run started at", datetime.now())
 ans = None
 input_path = "../input/" + (__file__.replace('py', 'txt') if "-t" not in sys.argv else "testcase.txt")
 
@@ -13,9 +15,15 @@ with open(input_path, "r") as f:
 
 # part a
 
-print("Answer A:", ans)
+print(datetime.now(), "\nAnswer A:", ans)
+timestamp_a = datetime.now()
 
 # part b
 
 print("Answer B:", ans)
+timestamp_b = datetime.now()
+
 sp.run("pbcopy", input=str(ans), text=True)
+
+print(timestamp_a)
+print(timestamp_b)
