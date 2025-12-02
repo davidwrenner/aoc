809f44171c6eb6798c4e0366b9eab1b845234a04
David Wrenner <davidwrenner@gmail.com>
2025-12-01 19:53:56 -0500

    initialize 2025

---
+++ 2025/src/template.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+import subprocess as sp
+import sys
+import numpy as np
+import re
+from datetime import datetime as dt
+from utils import *
+
+t_0 = dt.now()
+ans = None
+fp = "../input/" + (__file__.replace('py', 'txt') if "-t" not in sys.argv else "small.txt")
+
+with open(fp, "r") as f:
+    input = [line.rstrip() for line in f]
+
+# part a
+
+print("Answer A:", ans)
+t_a = dt.now()
+
+# part b
+
+print("Answer B:", ans)
+t_b = dt.now()
+
+sp.run("pbcopy", input=str(ans), text=True)
+
+print(t_0, t_a, t_b, sep="\n")
